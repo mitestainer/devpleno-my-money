0049a5432d410ceb79617d14da26fe7fd3b3fced
Matheus Mitestainer <matheusmitestainer@outlook.com>
2021-06-06 19:13:51 -0300

    Modulo 6

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import useGet from './useGet'
+
+const url = 'https://mymoney-e7e7b-default-rtdb.firebaseio.com/movimentacoes/2021-06.json'
 
 function App() {
+  const data = useGet(url)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My money</h1>
+      {data.loading && <p>Loading...</p>}
+      {JSON.stringify(data.data)}
     </div>
   );
 }
